6c4c964ac186a6f713f672b1a9d18c85d26b2914
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-08 09:07:14 +0530

    wrong JWT error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -15,6 +15,12 @@ module.exports = (err, req, res, next) => {
     const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
     err = new ErrorHandler(message, 400);
   }
+
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
   
   res.status(err.statusCode).json({
     success: false,
