bf4aa7abe1b7b13d21660872ef7a2833e0966805
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-09 09:00:22 +0530

    import order, product models and error handling utilities for controller setup

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,5 @@
+const Order = require("../models/orderModel");
+const Product = require("../models/productModel");
+const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
