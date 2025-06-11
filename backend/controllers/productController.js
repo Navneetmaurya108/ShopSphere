cd9918e5642fa595cd94d297dc5287ff9d7d3010
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-11 07:59:14 +0530

    import necessary packages

---
+++ backend/controllers/productController.js
@@ -0,0 +1,6 @@
+const Product = require("../models/productModel");
+const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
+const cloudinary = require("cloudinary");
+
