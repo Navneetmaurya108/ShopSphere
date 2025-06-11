b50a323a24e640aca5541d8c8f124af137f00d2d
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-11 08:53:00 +0530

    add pagination and structured response to get all products API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -41,6 +41,8 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
+const resultPerPage = 8;
+  const productsCount = await Product.countDocuments();
 
   const apiFeature = new ApiFeatures(Product.find(), req.query)
   .search()
@@ -50,5 +52,17 @@ let products = await apiFeature.query;
 
 let filteredProductsCount = products.length;
 
+
+  apiFeature.pagination(resultPerPage);
+
+  products = await apiFeature.query;
+
+  res.status(200).json({
+    success: true,
+    products,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  });
 
 });
