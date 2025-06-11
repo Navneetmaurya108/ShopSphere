f82eedd03d58e2347b4a23d2810658496ed56901
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-11 08:04:51 +0530

    create new product with uploaded image and user

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,3 +4,15 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ApiFeatures = require("../utils/apifeatures");
 const cloudinary = require("cloudinary");
 
+// Create Product -- Admin
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+
+  req.body.user = req.user.id;
+
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
