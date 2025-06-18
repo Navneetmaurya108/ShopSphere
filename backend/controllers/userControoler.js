cff6944e0897a9b7a198fd2403f105ef2834c263
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-18 21:49:27 +0530

    update user role (admin)

--- backend/controllers/userControoler.js
+++ backend/controllers/userControoler.js
@@ -219,3 +219,19 @@ exports.getAllUser = catchAsyncErrors(async (req, res, next) => {
     users,
   });
 });
+
+// Get single user (admin)
+exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHander(`User does not exist with Id: ${req.params.id}`)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
