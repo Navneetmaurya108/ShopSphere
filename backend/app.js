bf92e2c1f7719f88772b4b859701c7e5e265776b
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-21 22:27:12 +0530

    import necessary packages

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+
+const errorMiddleware = require("./middleware/error");
