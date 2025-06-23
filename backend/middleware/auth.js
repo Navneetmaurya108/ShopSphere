b5ef6d7e9307d9839dc5169e862ae314c53e8df8
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-23 23:09:59 +0530

    modify file name

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,4 @@
-const ErrorHander = require("../utils/errorhandler");
+const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
