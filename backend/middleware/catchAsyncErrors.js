77e4ebe2d6d363257785e444a37cdbc87d7c3e06
Navneet Maurya <navneetmaurya108@gmail.com>
2025-06-27 23:24:27 +0530

    add suitable message

--- backend/middleware/catchAsyncErrors.js
+++ backend/middleware/catchAsyncErrors.js
@@ -1,3 +1,5 @@
+// Wraps async route handlers to catch errors and pass them to Express error middleware
+
 module.exports = (theFunc) => (req, res, next) => {
   Promise.resolve(theFunc(req, res, next)).catch(next);
 };
