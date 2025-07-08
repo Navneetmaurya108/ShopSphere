cc79fc826efcd5dd61b8218e81cf759a9360e906
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-08 23:11:08 +0530

    Add cartReducer and initialize cart state from localStorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -20,6 +20,8 @@ import {
   userReducer,
 } from "./reducers/userReducer";
 
+import { cartReducer } from "./reducers/cartReducer";
+
 const reducer = combineReducers({
   products: productsReducer,
   productDetails: productDetailsReducer,
@@ -33,7 +35,19 @@ const reducer = combineReducers({
   forgotPassword: forgotPasswordReducer,
   allUsers: allUsersReducer,
   userDetails: userDetailsReducer,
+   cart: cartReducer,
 });
+
+let initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+    shippingInfo: localStorage.getItem("shippingInfo")
+      ? JSON.parse(localStorage.getItem("shippingInfo"))
+      : {},
+  },
+};
 
 const middleware = [thunk];
 
