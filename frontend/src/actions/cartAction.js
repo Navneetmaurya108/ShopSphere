750a162a47853216298d63efeded79d8879eba1c
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-09 21:12:38 +0530

    logic to remove items from the cart

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -23,3 +23,13 @@ export const addItemsToCart = (id, quantity) => async (dispatch, getState) => {
 
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
+
+// REMOVE FROM CART
+export const removeItemsFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: REMOVE_CART_ITEM,
+    payload: id,
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
