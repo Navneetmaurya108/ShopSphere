5409227dacc8233689743926300734a34ce0bfed
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-12 16:48:43 +0530

    logic for viewing orders

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -42,3 +42,19 @@ export const createOrder = (order) => async (dispatch) => {
     });
   }
 };
+
+// My Orders
+export const myOrders = () => async (dispatch) => {
+  try {
+    dispatch({ type: MY_ORDERS_REQUEST });
+
+    const { data } = await axios.get("/api/v1/orders/me");
+
+    dispatch({ type: MY_ORDERS_SUCCESS, payload: data.orders });
+  } catch (error) {
+    dispatch({
+      type: MY_ORDERS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
