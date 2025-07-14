e73d58ae5d2d05aaf81f02a07c5c64d646d64e97
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-14 20:38:39 +0530

    implement logout action to clear user session

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -88,3 +88,14 @@ export const loadUser = () => async (dispatch) => {
   }
 };
 
+// Logout User
+export const logout = () => async (dispatch) => {
+  try {
+    await axios.get(`/api/v1/logout`);
+
+    dispatch({ type: LOGOUT_SUCCESS });
+  } catch (error) {
+    dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message });
+  }
+};
+
