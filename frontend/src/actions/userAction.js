b7b6a73de0a75f06c35a5d9ed1e2256cbc9325e2
Navneet Maurya <navneetmaurya108@gmail.com>
2025-07-14 20:40:47 +0530

    logic to update password

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -116,3 +116,25 @@ export const updateProfile = (userData) => async (dispatch) => {
     });
   }
 };
+
+// Update Password
+export const updatePassword = (passwords) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_PASSWORD_REQUEST });
+
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.put(
+      `/api/v1/password/update`,
+      passwords,
+      config
+    );
+
+    dispatch({ type: UPDATE_PASSWORD_SUCCESS, payload: data.success });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
